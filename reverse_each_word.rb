8d1ab6535530ee01be022a55ada0d6a821259a70
thomas lombardo <tomlom1984@hotmail.co.uk>
2019-06-28 13:38:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word (sentence)
-    sentence.chars = Array
-    Array.reverse
-    Array = string.split(",")
-    Array.each do|string|
+    sentence.chars = array_string
+    array_string.reverse
+    array_string = string.split(",")
+    array_string.each do|string|
     end
 end
